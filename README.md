f9dfc2177fceb6ae7edba687bf6af20108a776f8
mmorrisonlk <mmorrisonlk@gmail.com>
2021-03-05 21:58:52 -0600

    Massive update. Using borrowed code that we adapted to fit our quiz. We have a functioning timer with color changing based on time remaining and a fail state

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Alright, new assignment, this time I need to build a website that can deliver a 
 So I have my mock up laid out in a way that I can visualize what ids I'll need. My next hurdle that I want to focus on is getting the Javascript to find and replace text properly. My idea is to get each of the texts correctly replacing individually and then try to get them to all replace the text at the same time.
 
 It looks like I have the basics set up and working. My next thought is how I will store the question and answers. My plan is to create an array of an array. By creating this correctly it should allow me to call an index that will select a question and the website should be able to use the question information because the questions, options and correct answers will all be stored at the same index in a given array. We will first work towards a working version of a simple array before adding multiple questions.
+
+I have successfully completed an array of all the questions (borrowed from the W3 schools javascript quiz). I also have the ability to cycle through these options. The next and (hopefully!) last hurdle is to get the timer working correctly.
--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var option4 = document.querySelector("#optionFour");
 
 var quizPosition;
 var quizStarted = false;
+var quizOver;
 
 var questions = [
     ["Inside which HTML element do we put the JavaScript?", "<js>", "<javascript>", "<scripting>", "<script>", 4],
@@ -20,82 +21,213 @@ var questions = [
     ["Which operator is used to assign a value to a variable?", "=", "*", "-", "x", 1]
 ]
 
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 30;
+const ALERT_THRESHOLD = 10;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const timeLimit = 60;
+let timePassed = 0;
+let timeLeft = 60;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="https://www.w3.org/TR/SVG2/">
+    <g class="base-timer__circle">
+        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+        <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+    </svg>
+    <span id="base-timer-label" class="base-timer__label">${formatTime(timeLeft)}
+    </span>
+</div>
+`;
+
+// document.getElementById("app").innerHTML = '...'
+
+function formatTime(time) {
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+}
+
+function startTimer() {
+    timerInterval = setInterval((countdown) => {
+        timePassed = timePassed += 1;
+        timeLeft = timeLimit - timePassed;
+    // document.getElementById("base-timer-label").innerHTML = formatTime(
+    //   timeLeft
+    // );
+    document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+    if (timeLeft <= 0) {
+        timesUp();
+    }
+    }, 1000);
+
+
+}
+
+function setRemainingPathColor(timeLeft) {
+    const { alert, warning, info } = COLOR_CODES;
+    if (timeLeft <= alert.threshold) {
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(warning.color);
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(alert.color);
+    } else if (timeLeft <= warning.threshold) {
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(info.color);
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(warning.color);
+    }
+  }
+
+function calculateTimeFraction() {
+    const rawTimeFraction = timeLeft / timeLimit;
+    return rawTimeFraction - (1 / timeLimit) * (1 - rawTimeFraction);
+}
+  
+function setCircleDasharray() {
+    const circleDasharray = `${(
+      calculateTimeFraction() * FULL_DASH_ARRAY
+    ).toFixed(0)} 283`;
+    document
+      .getElementById("base-timer-path-remaining")
+      .setAttribute("stroke-dasharray", circleDasharray);
+}
+
 option1.addEventListener("click", function(event){
-    if (quizStarted === false){
+    if (quizOver === true) {
+        return;
+    }
+    else if (quizStarted === false){
         fight();
     }
     else {
         if (questions [quizPosition][5] === 1) { 
-            console.log ("Correct");
             quizPosition ++;
             battleRound();
         }
         else {
-            console.log ("Wrong");
-            // timerPunishment();
+            option1.textContent = "Incorrect!";
+            timePenalty();
         }
     }
 });
 
 option2.addEventListener("click", function(event){
-    if (quizStarted === false){
-        alert("Click the answer button to start the quiz! Each incorrect answer costs you 5 seconds. Can you answer 13 questions in 60 seconds?");
+    if (quizOver === true) {
+        return;
+    }
+    else if (quizStarted === false){
+        alert("Click the answer button to start the quiz! Each incorrect answer costs you 5 seconds. Can you answer 10 questions in 60 seconds?");
     }
     else {
         if (questions [quizPosition][5] === 2) { 
-            console.log ("Correct");
             quizPosition ++;
             battleRound();
         }
         else {
-            console.log ("Wrong");
-            // timerPunishment();
+            option2.textContent = "Incorrect!";
+            timePenalty();
         }
     }
 });
 
 option3.addEventListener("click", function(event){
-    if (quizStarted === false){
+    if (quizOver === true) {
+        return;
+    }
+    else if (quizStarted === false){
         alert("Look at the current highscores!");
     }
     else {
         if (questions [quizPosition][5] === 3) { 
-            console.log ("Correct");
             quizPosition ++;
             battleRound();
         }
         else {
-            console.log ("Wrong");
-            // timerPunishment();
+            option3.textContent = "Incorrect!";
+            timePenalty();
         }
     }
 });
 
 option4.addEventListener("click", function(event){
-    if (quizStarted === false){
+    if (quizOver === true) {
+        return;
+    }
+    else if (quizStarted === false){
         alert("You can't run away!");
     }
     else {
         if (questions [quizPosition][5] === 4) { 
-            console.log ("Correct");
             quizPosition ++;
             battleRound();
         }
         else {
-            console.log ("Wrong");
-            // timerPunishment();
+            option4.textContent = "Incorrect!";
+            timePenalty();
         }
     }
 });
 
 function fight() 
 {
-    console.log("general");
     quizPosition = 0;
     quizStarted = true;
     battleRound();
-    // Starttimer
+    startTimer();
 };
+
+function timePenalty() {
+    timePassed = timePassed += 3
+}
+
+function timesUp() {
+    timeLeft = 0;
+    clearInterval(timerInterval);
+    document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
+    quizOver = true;
+    option1.textContent = "You were defeated by coding quiz!"
+    option2.textContent = "Wrong answers cost you 3 seconds"
+    option3.textContent = "You can do this!"
+    option4.textContent = "Reload the page to try again!"
+}
 
 function battleRound ()
 {
