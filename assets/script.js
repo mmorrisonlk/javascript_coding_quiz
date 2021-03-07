c7c75e7c70654c7039bd215a2dd137998b2b0cc4
mmorrisonlk <mmorrisonlk@gmail.com>
2021-03-06 22:40:31 -0600

    Fully fleshed out my commenting

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
+// First 5 variables are hooking variables to specific sections of the html base on matching IDs
 var question = document.querySelector("#question");
 var option1 = document.querySelector("#optionOne");
 var option2 = document.querySelector("#optionTwo");
 var option3 = document.querySelector("#optionThree");
 var option4 = document.querySelector("#optionFour");
+// This pulls out the high score string from local storage and turns it back into an array
 var theVeryBest = JSON.parse(localStorage.getItem('hallOfFame')) || [];
+// These are mostly logical variables that I reuse to pass through if statments throughout the document to tell it which code to execute
 var highScore;
 var quizPosition;
 var quizStarted = false;
 var quizOver;
-
+// This var I'm quite proud of. It stores an array of all the questions at specific indexes within that array. The final index is the correct answer. It could probably be cleaned up with keys to make it more readable for someone other than me. 
 var questions = [
     ["Inside which HTML element do we put the JavaScript?", "<js>", "<javascript>", "<scripting>", "<script>", 4],
     ["How do you write 'hello world' in an alert box?", "alert('Hello World');", "msg('Hello World');", "alertBox('Hello World');", "msgBox('Hello World');", 1],
@@ -78,7 +81,7 @@ function formatTime(time) {
     }
     return `${minutes}:${seconds}`;
 }
-
+// This starts the timer and tells it to run the code every second on the page. While also updating the current position on the timer circle and what color it should be.
 function startTimer() {
     timerInterval = setInterval((countdown) => {
         timePassed = timePassed += 1;
@@ -93,7 +96,7 @@ function startTimer() {
 
 
 }
-
+// Based on what time is remaining this cycles through CSS properties so the color changes as the timer runs out.
 function setRemainingPathColor(timeLeft) {
     const { alert, warning, info } = COLOR_CODES;
     if (timeLeft <= alert.threshold) {
@@ -112,7 +115,7 @@ function setRemainingPathColor(timeLeft) {
         .classList.add(warning.color);
     }
   }
-
+// Witchcraft which makes the timer work better
 function calculateTimeFraction() {
     const rawTimeFraction = timeLeft / timeLimit;
     return rawTimeFraction - (1 / timeLimit) * (1 - rawTimeFraction);
@@ -126,7 +129,11 @@ function setCircleDasharray() {
       .getElementById("base-timer-path-remaining")
       .setAttribute("stroke-dasharray", circleDasharray);
 }
-
+/* This I'm quite happy with. It is a generic click function that adapts to the current webpage state. If the game is over then it turns off the interaction (or responds by doing nothing might be more accurate).
+Next it checks if the quiz has been started yet and if it hasn't then it will start the quiz in the case of the first option.
+Then we have a new if statment that checks if the option chosen matches the answer index which advances to the next question.
+Else it changes the text to incorrect and subtracts 3 from the timer. It does keep applying the time penalty if they keep clicking incorrect.
+Maybe don't do that?*/
 option1.addEventListener("click", function(event){
     if (quizOver === true) {
         return;
@@ -145,7 +152,7 @@ option1.addEventListener("click", function(event){
         }
     }
 });
-
+// Similar but it gives a quick explanation if the quiz hasn't started
 option2.addEventListener("click", function(event){
     if (quizOver === true) {
         return;
@@ -164,7 +171,7 @@ option2.addEventListener("click", function(event){
         }
     }
 });
-
+// More of the same but it will pull up the highscore logic if the quiz has not started
 option3.addEventListener("click", function(event){
     if (quizOver === true) {
         return;
@@ -183,7 +190,7 @@ option3.addEventListener("click", function(event){
         }
     }
 });
-
+// This is just for my amusement and to drive home the pokemon analogy
 option4.addEventListener("click", function(event){
     if (quizOver === true) {
         return;
@@ -202,7 +209,7 @@ option4.addEventListener("click", function(event){
         }
     }
 });
-
+// This function starts the quiz and ensures that the website is at the right question index, knows that the in quiz logic should execute, asks the first question and then starts the timer.
 function fight() 
 {
     quizPosition = 0;
@@ -210,11 +217,13 @@ function fight()
     battleRound();
     startTimer();
 };
-
+/* Any incorrect selections during the quiz run this method which increments timePassed by 3 in addition to the regular increment every second.
+I am aware of the fact that this probably ends up eating seconds depending on when this is run in comparison to the regular interval incrementation
+¯\_(ツ)_/¯ */
 function timePenalty() {
     timePassed = timePassed += 3
 }
-
+// Most importantly for my sanity it stops the interval from running and sets the time left to 0 so you "theoretically" can't get it to stick on displaying a negative time value
 function timesUp() {
     timeLeft = 0;
     clearInterval(timerInterval);
@@ -225,7 +234,13 @@ function timesUp() {
     option3.textContent = "You can do this!"
     option4.textContent = "Reload the page to try again!"
 }
-
+/* This is one of my work horse functions. It most relevantly handles updating all the text content with the current question and answers.
+I also gave it the bonus purpose of handling the end of the quiz functions. Because there is nothing declared after the 
+final array with questions and answers it defaults to null and we can use this defaut behavior to tell the quiz that the quiz is over.
+It also prompts the user to enter their name and appends that to the high score array with the completion time and then turn that into 
+a string in local storage. One fun thing is that I just copied quizOver = true from my fail state on a whim without a real purpose.
+Only to realize that it also succeds in turning off the quiz logic until reloaded. So go accidental competency!
+*/
 function battleRound (){
 if (questions [quizPosition] == null){
     clearInterval(timerInterval);
@@ -244,7 +259,7 @@ else {
 
 }
 };
-
+// The end! This explains the function of the 3rd button if there are no high scores. And displays the high scores if there are any. It doesn't organize them ¯\_(ツ)_/¯ but that is a problem for future Kelvin
 function theBestAround () {
     if (theVeryBest == 0){
         alert("This is where the best times go!");
